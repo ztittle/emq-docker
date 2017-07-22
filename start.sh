25493e77f0c453813329e667e1d7bebcc5754131
turtled <turtled@emqtt.io>
2017-07-22 12:08:22 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -127,51 +127,7 @@ fi
 
 ## EMQ Main script
 
-# Start and run emqttd, and when emqttd crashed, this container will stop
+# Start and run emqttd
 
-/opt/emqttd/bin/emqttd foreground &
+/opt/emqttd/bin/emqttd start
 
-# wait and ensure emqttd status is running
-WAIT_TIME=0
-while [[ -z "$(/opt/emqttd/bin/emqttd_ctl status |grep 'is running'|awk '{print $1}')" ]]
-do
-    sleep 1
-    echo "['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:waiting emqttd"
-    WAIT_TIME=$((WAIT_TIME+1))
-    if [[ $WAIT_TIME -gt 5 ]]; then
-        echo "['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:timeout error"
-        exit 1
-    fi
-done
-
-echo "['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:emqttd start"
-
-# Change admin password
-
-if [[ ! -z "$EMQ_ADMIN_PASSWORD" ]]; then
-    echo "['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:admin password changed to $EMQ_ADMIN_PASSWORD"
-    /opt/emqttd/bin/emqttd_ctl admins passwd admin $EMQ_ADMIN_PASSWORD &
-fi
-
-# monitor emqttd is running, or the docker must stop to let docker PaaS know
-# warning: never use infinite loops such as `` while true; do sleep 1000; done`` here
-#          you must let user know emqtt crashed and stop this container,
-#          and docker dispatching system can known and restart this container.
-IDLE_TIME=0
-while [[ $IDLE_TIME -lt 5 ]]
-do  
-    IDLE_TIME=$((IDLE_TIME+1))
-    if [[ ! -z "$(/opt/emqttd/bin/emqttd_ctl status |grep 'is running'|awk '{print $1}')" ]]; then
-        IDLE_TIME=0
-    else
-        echo "['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:emqttd not running, waiting for recovery in $((25-IDLE_TIME*5)) seconds"
-    fi
-    sleep 5
-done
-# If running to here (the result 5 times not is running, thus in 25s emq is not running), exit docker image
-# Then the high level PaaS, e.g. docker swarm mode, will know and alert, rebanlance this service
-
-# tail $(ls /opt/emqttd/log/*)
-
-echo "['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:emqttd exit abnormally"
-exit 1
